02b25d3f55bad07423c9efa4913ccb9eccf18dcf
Timpartage1 <tim58841@gmail.com>
2024-11-22 15:24:53 +0200

    first version 1.0.0 picasf local

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
-const user = require('./models/user');
+
 //rendering views 
 app.set('view engine', 'ejs');
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 //db
 const moongoose = require('mongoose');
@@ -19,46 +19,17 @@ moongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log(err);
     });
 
-const Users = new user({
-    firstname: "Timothee",
-    lastname: "Katende",
-    age: "24",
-    password: "12345678"
-});
-
-
-app.get('/add-user', (req, res) => {
-
-    Users.save().then(result => res.send(result));
-
-
-});
-
-
-app.get('/allUser', (req, res) => {
-    User.find().then(result => res.send(result));
-});
-
-const blog = [{ 'title': 'Ruch Dad', 'content': 'lorem ipsum' },
-{ 'title': 'Yanik Noad', 'content': 'Il est venu ce amtin ipsum' },
-{ 'title': 'Yanke Boeh', 'content': 'lorem ipsum' }];
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home', blog });
+    res.render('index');
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile('./about.html', { root: __dirname });
-});
 
-app.get('/about-us', (req, res) => {
-    res.redirect('./about');
-});
 
 
 
 app.use((req, res) => {
 
-    res.sendFile('error.html', { root: __dirname });
+    res.send('Not found')
 
 });
